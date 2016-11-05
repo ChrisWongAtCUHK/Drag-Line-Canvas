7787f889119c1a47a377da110193931c3cacf762
chriswongatcuhk <wc38.wong@gmail.com>
2016-11-05 23:01:33 +0800

    Find a up word.

--- words.js
+++ words.js
@@ -1,30 +1,10 @@
-/*
- *			+ + + + + + + + + +  
- *			+ + t + e + + + + +  
- *			+ + a p p l e + + +  
- *			+ + c + + + g + + +  
- *			+ + + + d + + n + +  
- *			+ + + + o + g + u +  
- *			+ + e g g + + i + j  
- *			+ c + + + + y + r +  
- *			i + + e m o h + + l  
- *			+ + + + b + + + + +  
- *
- *
- * */
-
-/*
- * Start the words
- * */
-function startWords(){
-	var words = ['apple', 'boy', 'cat', 'dog', 'egg', 'girl', 'home', 'ice', 'jungle'];
-	return words;
-}
+var words;
 
 /*
  * Start the puzzle 
  */
 function startPuzzle(){
+	words = ['apple', 'boy', 'cat', 'dog', 'egg', 'girl', 'home', 'ice', 'jungle'];
 	var puzzle = 
 		[
 			'+', '+', '+', '+', '+', '+', '+', '+', '+', '+',  
@@ -51,18 +31,69 @@ function startPuzzle(){
 	}
 }
 
-function getCellLetter(cell){
-	var letter = $($('#gameGrid td')[cell.y * 10 + cell.x])[0].innerHTML;
+/*
+ *	Get a single letter from the grid
+ * */
+function getCellLetter(gameGrid, x, y){
+	var letter = $(gameGrid[y * 10 + x])[0].innerHTML;
 	return letter;
 }
 
 /*
- *
+ *	Get the word, there are 8 directions
+ * */
+function getWord(gameGrid, startCell, endCell){
+	var word = '';
+	if(startCell.x == endCell.x && startCell.y > endCell.y){
+		// up
+		for(var j = startCell.y; j >= endCell.y; j--){
+			word += getCellLetter(gameGrid, startCell.x, j);
+		}
+	} else if(startCell.x < endCell.x && startCell.y > endCell.y){
+		// up-right
+		return 1;
+	} else if(startCell.x < endCell.x && startCell.y == endCell.y){
+		// right
+		return 2;
+	} else if(startCell.x < endCell.x && startCell.y < endCell.y){
+		// down-right
+		return 3;
+	} else if(startCell.x == endCell.x && startCell.y < endCell.y){
+		// down
+		return 4;
+	} else if(startCell.x < endCell.x && startCell.y < endCell.y){
+		// down-left
+		return 5;
+	} else if(startCell.x < endCell.x && startCell.y == endCell.y){
+		// left
+		return 6;
+	} else if(startCell.x < endCell.x && startCell.y > endCell.y){
+		// up-left
+		return 7;
+	}
+	return word;
+}
+
+/*
+ *	Check if the word is in list, remove it if it is found
+ * */
+function isWordInList(word){
+	for(var i = 0; i < words.length; i++){
+		if(word == words[i]){
+			words.splice(i ,1);
+			return true;
+		}
+	}
+}
+
+/*
+ *	Deter if the word is matched
  * */
 function isMatch(startCell, endCell){
-	var startLetter = getCellLetter(startCell);
-	console.log(startLetter);
-	var endLetter = getCellLetter(endCell);
-	console.log(endLetter);
-	return true;
+	var gameGrid = $('#gameGrid td');
+	var word = getWord(gameGrid, startCell, endCell);
+	if(isWordInList(word)){
+		return true;
+	}
+	return false;
 }
